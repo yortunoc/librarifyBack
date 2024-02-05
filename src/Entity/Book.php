32d668ab49d2b480147c3995f6e30db2fc99cb20
yury ortuno calvo <yortuno@buk.cl>
2024-02-05 17:46:51 -0400

    add feture to delete actions

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -1,12 +1,13 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=BookRepository::class)]
+ * @ORM\Entity(repositoryClass=BookRepository::class)
  */
 class Book
 {
@@ -16,42 +17,71 @@ class Book
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
-     * @ORM\Column(length=255)
+     * @ORM\Column(type="string", length=255)
      */
     private $title;
+    /**
+     * @ORM\Column(type="string", length=512, nullable=true)
+     */
+    private $image;
 
     /**
-     * @ORM\Column(length=512, nullable=true)
+     * @ORM\ManyToMany(targetEntity=Category::class, inversedBy="books")
      */
-    private $image = null;
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getTitle(): ?string
     {
         return $this->title;
     }
-
     public function setTitle(string $title): self
     {
         $this->title = $title;
-
         return $this;
     }
-
     public function getImage(): ?string
     {
         return $this->image;
     }
-
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Category[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Category $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Category $category): self
+    {
+        if ($this->categories->contains($category)) {
+            $this->categories->removeElement($category);
+        }
 
         return $this;
     }
